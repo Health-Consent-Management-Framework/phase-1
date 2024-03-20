6f95de459b2467a411eecd5e96ebd801eaa7659a
Vivek <20bcs105@iiitdwd.ac.in>
2024-03-20 12:03:53 +0530

    worker details setup

--- client/src/components/forms/addWorker.tsx
+++ client/src/components/forms/addWorker.tsx
@@ -10,18 +10,33 @@ import { useCombinedContext } from "../../store"
 
 export const AddWorker:React.FC = ()=>{
     const navigate = useNavigate()
-    const {wallet,web3} = useCombinedContext()
-    const workerContract = useContract(workerABI,workerNetwork)
+    const {wallet,updateNotification} = useCombinedContext()
+    const contract = useContract(workerABI,workerNetwork)
     const [loading,setLoading] = useState(false)
-    function handleSubmit() {
-        console.log("")
-    }
 
-    function handleSubmit(e){
-        setLoading(true)
-        const {fname,lname,email,DoB,facilityId} = e.target;
+
+    const handleSubmit = async (e) => {
+        try {
+          e.preventDefault();
+          setLoading(true)
+          // console.log(contract);
+          if(contract) {
+            const {fname,lname,DoB,phoneno,address,email,gender,height,weight} = e.target
+            const [day,month,year] = DoB.value.split('-').map(ele=>ele)
+            console.log(fname.value,lname.value,email.value,phoneno.value,day,month,year,address.value,params.id)
+            const transaction = await contract?.methods.createWorker(fname.value,lname.value,email.value,phoneno.value,day,month,year,address.value,params.id).send({ from: wallet.accounts[1],gas:"1000000" });
+            console.log(transaction)
+            const events = transaction.events
+            if(Object.keys(events).includes('PatientCreated')){
+              updateNotification({type:'success',message:"User created successfully"})
+              navigate('/')
+            }
+          }
+        } catch (error) {
+          console.error('Error submitting transaction', error);
+        }
         setLoading(false)
-    }
+      };
 
     return(
         <section className="m-auto flex items-center flex-col justify-center pt-10">
@@ -33,15 +48,21 @@ export const AddWorker:React.FC = ()=>{
                         <LabeledInput outlineColor="red" textStyle="text-red-700 capitalize" label="last name" name="lname"/>
                     </div>
                         <LabeledInput textStyle="text-red-700 capitalize" label="Email" name="username"/>
-                    <div className="flex items-center justify-center duration-500 flex-wrap md:flex-nowrap">
+                    <div className='flex gap-5'>
+                        <LabeledInput textStyle="text-red-700 capitalize" type="text" name="phoneno" label="phone number"/>
                         <LabeledInput textStyle="text-red-700 capitalize" type="date" name="DoB" label="date of birth"/>
+                        {/* <LabeledInput textStyle='text-red-700 capitalize' label='pincode' name='pincode'/> */}
                     </div>
-                    <div className="flex justify-center">
-                        <LabeledSelect textStyle="text-red-700 capitalize" name="facilityId" label="Facility" options={wallet.accounts.map(ele=>({value:ele,name:ele}))} />
+                    <div className='flex gap-5'>
+                        <LabeledSelect textStyle='text-red-700 capitalize' name="Gender" label="gender" options={[{value:'M',name:'Male'},{value:'F',name:'female'}]} />
+                        <LabeledInput textStyle='text-red-700 capitalize' label='Height' name='height'/>
+                        <LabeledInput textStyle='text-red-700 capitalize' label='Weight' name='weight'/>
+                    </div>
+                    <div className="flex items-center justify-center duration-500 flex-wrap md:flex-nowrap">
+                        <LabeledInput textStyle='text-red-700 capitalize' name='address' label='Address'/>
                     </div>
                     <div className={`flex gap-3 justify-center pt-3`}>
-                        <Button className="bg-red-500 text-white hover:border-red-700 hover:bg-red-300" buttonType="primary" loader={loading}>Add Worker</Button>
-                        <Button className="hover:border-red-700 hover:text-red-700" onClick={()=>navigate('/login')}>Clear</Button>
+                        <Button className="bg-red-500 text-white hover:border-red-700 hover:bg-red-300" loader={loading}>Add Worker</Button>
                     </div>
             </form>
         </section>
